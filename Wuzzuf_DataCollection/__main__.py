92cdbef0559fba77f02864718d0d2cbd1a73c771
SamehAmnoun <samehamnoun@gmail.com>
2020-10-08 16:42:18 +0200

    feat: added multiple parts in jobJson
    Added:
    - Job Roles: A list of roles ex: IT/Software Development
    - Job Requirements 
    - About the Job
    - Keywords: A list of key words ex: Game Design

--- Wuzzuf_DataCollection/__main__.py
+++ Wuzzuf_DataCollection/__main__.py
@@ -21,9 +21,9 @@ jobJson["City"] =  str(tree.xpath('/html/body/div[4]/div/div[1]/div[1]/div[1]/di
 # "Salary": "Confidential",
 # "Vacancies": "4 open positions",
 # "EducationLevel": "Bachelor's Degree",
-# "Job Roles": [
-# "AbouttheJob": "We are looking for a game designer with a passion for all genres of games, a knack for coding and storytelling, and an understanding of the industry, market, and target audiences.     Game designers can expect to manage teams of programmers, artists, animators, and sound engineers, as well as plan schedules and work with budgets.     Their responsibilities also include developing design and gaming protocols, defining game-play mechanics, coordinating with other game designers, ensuring quality, and meeting with company executives.     Main Job Duties: Creating innovative games for entertainment or education purposes. Conceptualizing and developing characters, rules, settings and stories for new games. Pitching new game ideas to executives and clients. Managing multiple teams and projects. Prototyping new games. Following industry trends and good practices. Monitoring work and cash flows. Developing design and gaming protocols. Doing quality control.",
-# "Job Requirements": "Understanding of mobile apps infrastructure    Knowledge of the JavaScript, TypeScript -including ES6+ syntax- and its nuances     Familiarity with native build tools, like XCode, Android Studio, Gradle, ..etc.     Understanding of REST/Graph APIs, the document request model, and offline storage   Experience with third-party libraries and APIs such as Google Maps API, Scandit API, SQLite API, etc.    Rock solid at working with third-party dependencies and debugging dependency conflicts     Experience with Git Source Control.     Good understanding of mobile application design patterns.   Experience connecting/calling RESTful services.   Excellent problem solving, analytical, and troubleshooting skills.  Knowledge of UI/UX design principles and data visualization to create a polished, intuitive user experience is strongly desired.",
-# "Keywords": [
+jobJson["Job Roles"] =  [str(role.text_content()).strip() for role in tree.xpath('/html/body/div[4]/div/div[1]/div[2]/div[2]/div/div')]
+jobJson["About The Job"] = '\n'.join([str(role.text_content()).strip() for role in tree.xpath('/html/body/div[4]/div/div[1]/div[2]/span/ul[1]/li')])
+jobJson["Job Requirements"] =  '\n'.join([str(role.text_content()).strip() for role in tree.xpath('/html/body/div[4]/div/div[1]/div[3]/span/ul/li')])
+jobJson["Keywords"] = [str(role.text_content()).strip() for role in tree.xpath('/html/body/div[4]/div/div[1]/div[3]/div[2]/div/div')]
 
 print(jobJson)
