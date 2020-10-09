777d5b63b5454b26a35dc17f2ed91da813b6a646
DigitalPhoenixX <mohamed.xyz@hotmail.com>
2020-10-09 22:18:45 +0200

    feat: delay between requests for job info

--- Wuzzuf_DataCollection/__main__.py
+++ Wuzzuf_DataCollection/__main__.py
@@ -1,10 +1,12 @@
 import json
+import time
 from pathlib import Path
 
 from Wuzzuf_DataCollection.getjobinfo import getJobInfo
 from Wuzzuf_DataCollection.getjoblinks import getJobLinks
 
 OUTPUT_FOLDER = "output"
+TIME_BETWEEN_REQUESTS = 2.5
 
 Path(OUTPUT_FOLDER).mkdir(parents=True, exist_ok=True)
 
@@ -15,7 +17,10 @@ with open(f"{OUTPUT_FOLDER}/links.json", "w") as outputFile:
 
 
 for i, link in enumerate(jobLinks):
+    print(f"Job # {i}")
     jobInfo = getJobInfo(link)
+
+    time.sleep(TIME_BETWEEN_REQUESTS)
 
     with open(f"{OUTPUT_FOLDER}/{i}.json", "w") as outputFile:
         json.dump(jobInfo, outputFile)
