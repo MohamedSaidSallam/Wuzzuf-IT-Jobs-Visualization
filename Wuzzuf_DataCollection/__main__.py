abc977e1404890c4a268c2c43578c626ce6348d1
DigitalPhoenixX <mohamed.xyz@hotmail.com>
2020-10-29 18:28:29 +0200

    feat: command line args

--- Wuzzuf_DataCollection/__main__.py
+++ Wuzzuf_DataCollection/__main__.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import os.path
 import time
@@ -12,39 +13,117 @@ JOBS_OUTPUT_FOLDER = f"{OUTPUT_FOLDER}/jobs"
 OUTPUT_LINKS_FILE = f"{OUTPUT_FOLDER}/links.json"
 TIME_BETWEEN_REQUESTS = 2.5
 
+
 def formattedJsonDumps(data, file):
     json.dump(data, file, indent=4, sort_keys=True)
 
-logger.debug("Started")
 
-logger.debug(f"create output folder if not present @ {OUTPUT_FOLDER}")
-Path(OUTPUT_FOLDER).mkdir(parents=True, exist_ok=True)
-logger.debug(f"create jobs output folder if not present @ {JOBS_OUTPUT_FOLDER}")
-Path(JOBS_OUTPUT_FOLDER).mkdir(parents=True, exist_ok=True)
+def main(use_existing_Links_file, linksStartIndex, linksEndIndex, createCSV, getJobInfo):
+    logger.debug("Started")
 
-if os.path.isfile(OUTPUT_LINKS_FILE):
-    logger.debug(f"{OUTPUT_LINKS_FILE} found")
-    with open(OUTPUT_LINKS_FILE, "r") as outputFile:
-        jobLinks = json.load(outputFile)
-else:
-    logger.debug(f"{OUTPUT_LINKS_FILE} not found; getting links")
-    jobLinks = getJobLinks()
+    logger.debug(f"create output folder if not present @ {OUTPUT_FOLDER}")
+    Path(OUTPUT_FOLDER).mkdir(parents=True, exist_ok=True)
+    logger.debug(
+        f"create jobs output folder if not present @ {JOBS_OUTPUT_FOLDER}")
+    Path(JOBS_OUTPUT_FOLDER).mkdir(parents=True, exist_ok=True)
 
-    logger.debug(f"writing {OUTPUT_LINKS_FILE}")
-    with open(OUTPUT_LINKS_FILE, "w") as outputFile:
-        formattedJsonDumps(jobLinks, outputFile)
+    if use_existing_Links_file:
+        if os.path.isfile(OUTPUT_LINKS_FILE):
+            logger.debug(f"using Existing Links JSON")
+            with open(OUTPUT_LINKS_FILE, "r") as outputFile:
+                jobLinks = json.load(outputFile)
+        else:
+            logger.critical(f"links JSON file not found @{OUTPUT_LINKS_FILE}")
+            exit(-1)
+    else:
+        logger.debug(f"getting jobs links")
+        jobLinks = getJobLinks()
+
+        logger.debug(f"writing job {OUTPUT_LINKS_FILE}")
+        with open(OUTPUT_LINKS_FILE, "w") as outputFile:
+            formattedJsonDumps(jobLinks, outputFile)
+
+    if getJobInfo:
+        if linksEndIndex == -1:
+            linksEndIndex = len(jobLinks)
+
+        logger.debug(
+            f"going through links list from {linksStartIndex} to {linksEndIndex}")
+        for i, link in enumerate(jobLinks[linksStartIndex:linksEndIndex]):
+            logger.debug(f"Getting Info for Job # {i}/{len(jobLinks)}")
+            jobInfo = getJobInfo(link)
+
+            logger.debug(f"Going to sleep")
+            time.sleep(TIME_BETWEEN_REQUESTS)
+
+            logger.debug(f"Writing File to {JOBS_OUTPUT_FOLDER}/{i}.json")
+            with open(f"{JOBS_OUTPUT_FOLDER}/{i}.json", "w") as outputFile:
+                formattedJsonDumps(jobInfo, outputFile)
+    else:
+        logger.debug(f"creating job info JSON was disabled by command args")
+
+    if createCSV:
+        pass
+    else:
+        logger.debug(f"creating CSV was disabled by command args")
+
+    logger.debug(f"DONE!!")
 
 
-logger.debug(f"going through links list")
-for i, link in enumerate(jobLinks):
-    logger.debug(f"Getting Info for Job # {i}/{len(jobLinks)}")
-    jobInfo = getJobInfo(link)
+parser = argparse.ArgumentParser(
+    description=("Gets the list of Job offers on wuzzuf.com for it, gets the details of each offer then generates a CSV file with all the jobs"
+                 "\nWarning: the output file is overwritten with each run!!"),
+    epilog="https://github.com/TheDigitalPhoenixX/Wuzzuf-IT-Jobs-Visualization"
+)
 
-    logger.debug(f"Going to sleep")
-    time.sleep(TIME_BETWEEN_REQUESTS)
+parser.add_argument("-l", "--use-existing-Links-file",
+                    help="Use the existing links JSON file (default: %(default)s)",
+                    default=False,
+                    action="store_true")
 
-    logger.debug(f"Writing File to {JOBS_OUTPUT_FOLDER}/{i}.json")
-    with open(f"{JOBS_OUTPUT_FOLDER}/{i}.json", "w") as outputFile:
-        formattedJsonDumps(jobInfo, outputFile)
 
-logger.debug(f"DONE!!")
+def positiveInt(string):
+    value = int(string)
+    if value < 0:
+        raise argparse.ArgumentTypeError(
+            f"{string} is not a index (not a positive integer)")
+    return value
+
+
+parser.add_argument("-i", "--start-index",
+                    help="Start index in links JSON to start getting job info (default: %(default)s)",
+                    default=0,
+                    type=positiveInt,
+                    action="store")
+
+parser.add_argument("-e", "--end-index",
+                    help="Start index in links JSON to start getting job info",
+                    default=-1,
+                    type=positiveInt,
+                    action="store")
+
+parser.add_argument("-c", "--skip-create-csv",
+                    help="Create CSV files combining data from Job JSONs (default: %(default)s)",
+                    default=False,
+                    action="store_true")
+
+parser.add_argument("-f", "--skip-get-jobs-info",
+                    help="Create JSON files for each job (or jobs within the start and end index if specified) in links JSON file (default: %(default)s)",
+                    default=False,
+                    action="store_true")
+
+args = parser.parse_args()
+
+if args.start_index >= args.end_index:
+    logger.critical(
+        "end index ({args.end_index}) is smaller than or equal start index ({args.start_index})")
+    exit(-1)
+
+
+main(
+    use_existing_Links_file=args.use_existing_Links_file,
+    linksStartIndex=args.start_index,
+    linksEndIndex=args.end_index,
+    createCSV=not args.skip_create_csv,
+    getJobInfo=not args.skip_get_jobs_info,
+)
