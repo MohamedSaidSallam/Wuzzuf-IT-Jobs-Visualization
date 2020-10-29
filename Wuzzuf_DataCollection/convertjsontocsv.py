84661187feee25fdeec5640a7d0f7a7b275b780f
DigitalPhoenixX <mohamed.xyz@hotmail.com>
2020-10-29 21:08:42 +0200

    fix: job csv not escaped comma and wrong data order

--- Wuzzuf_DataCollection/convertjsontocsv.py
+++ Wuzzuf_DataCollection/convertjsontocsv.py
@@ -10,22 +10,16 @@ OUTPUT_FOLDER = "output"
 def makeJobsCSV():
     with open(f"{OUTPUT_FOLDER}/jobs.csv", "w", newline='', encoding='utf-8') as jobsCSV:
         csvWriter = csv.writer(jobsCSV, delimiter=',', escapechar='"')
-        csvWriter.writerow(["Key", "Title", "Company", "City", "PostedDate", "ExperienceNeeded", "CareerLevel",
-                            "JobType", "Salary", "Vacancies", "Education Level", "About The Job", "Job Requirements"])
+        keys=["Key", "title", "Company", "City", "Posted Date", "Experience Needed", "Career Level",
+                            "Job Type", "Salary", "Vacancies", "Career Level", "About The Job", "Job Requirements", "link"]
+        csvWriter.writerow(keys)
         for filename in os.listdir(JOBS_FOLDER):
             if os.path.isfile(os.path.join(JOBS_FOLDER, filename)):
                 if os.path.splitext(filename)[1][1:] == 'json':
                     with open(f"{JOBS_FOLDER}/{filename}", "r") as job:
                         jobData = json.load(job)
-                        jobData.pop("Keywords", None)
-                        jobData.pop("Job Roles", None)
-                        jobData['About The Job'] = jobData['About The Job'].replace(
-                            '\n', '     ')
-                        jobData['Job Requirements'] = jobData['Job Requirements'].replace(
-                            '\n', '     ')
-                        csvRow = [f"{jobData['title'] + jobData['Company']}"]
-                        csvRow.extend(jobData.values())
-                        csvWriter.writerow(csvRow)
+                        jobData["Key"] = f"{jobData['title'] + jobData['Company']}"
+                        csvWriter.writerow([jobData[currentKey] for currentKey in keys])
 
 
 def makeKeywordsCSV():
